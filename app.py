b3c0485544a10521e1d5e56d8ec65fac619088e9
Srishti Saha <srishti280992@yahoo.com>
2020-02-29 21:08:45 -0500

    added app files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'This site has been visited {} times.\n'.format(count)
+    
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
