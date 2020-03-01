c8992bda2e83ee10d7d32d0d13f3504ee6fd4db1
Srishti Saha <srishti280992@yahoo.com>
2020-02-29 21:53:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from redis import Redis
+import logging
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'This site has been visited {} times.\n'.format(count)
-    
+    return 'This site has been visited {} times.\n Refresh to see a change!'.format(count)
+
+
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
@@ -16,6 +19,7 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
