99bccae2a4adb957e19dcaf8cfb5dc5d8c1a4d4b
Srishti Saha <srishti280992@yahoo.com>
2020-03-01 08:22:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
-from redis import Redis
+import time
+import redis
 import logging
+from flask import Flask
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+counter = redis.Redis(host='redis', port=6379)
 
+def visits_count():
+    retries = 5
+    while True:
+        try:
+            return counter.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
+    count = visits_count()
     return 'This site has been visited {} times.\n Refresh to see a change!'.format(count)
 
 
